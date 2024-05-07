14433c6a730a9d8d31793ddfb81ef70a49a91dde
Chauncey Darby <chaunceydrb@gmail.com>
2024-05-07 23:56:31 +0200

    updated-menu

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,8 +1,12 @@
 import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets/assets'
 
 const ExploreMenu = () => {
   return (
-    <div>ExploreMenu</div>
+    <div className='explore-menu' id='explore-menu'>
+        <h1>Explore our menu</h1>
+        <p>Choose from a</p>
+    </div>
   )
 }
 
