09c5f3f2dc03e7c6d0207431908942841e9157c0
Chauncey Darby <chaunceydrb@gmail.com>
2024-05-08 23:04:40 +0200

    updated

--- src/components/AppDownload/AppDowload.jsx
+++ src/components/AppDownload/AppDowload.jsx
@@ -3,7 +3,7 @@
 const AppDowload = () => {
   return (
     <div className="app-download" id="app-download">
-        <p></p>
+        <p>For a Great Experiance Download <br /> Gobble App</p>
     </div>
   )
 }
