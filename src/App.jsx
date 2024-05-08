5fa0a41b6b305021a18cc06182c7c62834081fde
Chauncey Darby <chaunceydrb@gmail.com>
2024-05-08 14:45:34 +0200

    updated-footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,22 @@ import Navbar from './components/NavBar/Navbar'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
-    <div className='app'>
+
+    <>
+      <div className='app'>
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/cart' element={<Cart/>} />
         <Route path='/order' element={<PlaceOrder/>}/>
-      </Routes>
-    </div>
+        </Routes>
+       </div>
+      <Footer/>
+    </>
   )
 }
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,40 @@
+import { assets } from '../../assets/assets/assets'
 import './Footer.css'
 
 const Footer = () => {
   return (
-    <div className='footer'>
-
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img className='footer-logo'src={assets.logo2} alt="" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum porro error consectetur sed quos placeat debitis at doloribus qui odio! Vitae cupiditate, laborum ipsam nam neque dolorem veniam nesciunt fuga?</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="facebook icon" />
+                    <img src={assets.twitter_icon} alt="twitter icon" />
+                    <img src={assets.linkedin_icon} alt="linkedin icon" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GIVE US A SHOUT!</h2>
+                <ul>
+                    <li>+34-211-867-5309</li>
+                    <li>contact@deeznutz.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr /> 
+        <p className='footer-copyright'>
+            Copyright 2024 © Gobble.com - All Right Reserved.
+        </p>
     </div>
   )
 }
