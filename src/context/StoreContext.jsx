ebc78e32b2d4565d1387a5d9dbd04f616d24e0e5
Chauncey Darby <chaunceydrb@gmail.com>
2024-05-08 11:17:47 +0200

    updated-FoodDisplay

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,12 +1,15 @@
+/* eslint-disable react/prop-types */
 import { createContext } from "react";
+import { food_list } from "../assets/assets/assets";
 
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
 
     const contextValue = {
-
+        food_list
     }
+    
     return (
         <StoreContext.Provider value={contextValue}>
             {props.children}
