04b60eb5db6bd19f004bbb2d7670a0c309f299ef
Chauncey Darby <chaunceydrb@gmail.com>
2024-05-08 12:45:24 +0200

    updated-navbar logo

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 
 import { useContext } from 'react'
 import './FoodDisplay.css'
--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import { assets } from '../../assets/assets/assets'
 import './FoodItem.css'
 
