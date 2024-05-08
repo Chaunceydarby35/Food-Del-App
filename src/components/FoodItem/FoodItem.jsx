d6135a32576d2ee48b52efbfb62319fc614c829c
Chauncey Darby <chaunceydrb@gmail.com>
2024-05-08 13:00:26 +0200

    updated

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,13 +1,24 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
+import { useState } from 'react'
 import { assets } from '../../assets/assets/assets'
 import './FoodItem.css'
 
 const FoodItem = ({id,name,price,description,image}) => {
+
+    const [itemCount,setItemCount] = useState(0)
+
+
   return (
     <div className='food-item'>
         <div className="food-item-image-container">
             <img src={image} alt="" className="food-item-image" />
+            {!itemCount
+              ?<img className='add' onClick={() =>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt="" />
+              :<div className='food-item-counter'>
+                
+              </div>
+            }
         </div>
         <div className="food-item-info">
             <div className="food-item-name-rating">
