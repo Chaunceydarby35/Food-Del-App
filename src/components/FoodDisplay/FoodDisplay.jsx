37e67a68759a7da5bce1c2434cb7f536f9c19c23
Chauncey Darby <chaunceydrb@gmail.com>
2024-05-08 12:26:36 +0200

    updated

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -2,8 +2,9 @@
 import { useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../FoodItem/FoodItem'
 
-const FoodDisplay = ({}) => {
+const FoodDisplay = ({category}) => {
 
   const {food_list} = useContext(StoreContext)
 
@@ -12,7 +13,7 @@ const FoodDisplay = ({}) => {
         <h2>Top dishes near you!</h2>
         <div className="food-display-list">
             {food_list.map((item,index)=>{
-                return
+                return <FoodItem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image}/>
             })}
         </div>
     </div>
