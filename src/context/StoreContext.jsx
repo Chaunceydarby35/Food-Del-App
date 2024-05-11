f88693687aaa9d6ad4093044ba9beb48398cb795
Chauncey Darby <chaunceydrb@gmail.com>
2024-05-11 17:26:47 +0200

    updated

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,10 +1,23 @@
 /* eslint-disable react/prop-types */
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets/assets";
 
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
+
+const [cartItems,setCartItems] = useState({})
+
+    const addToCart = (itemId) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev)=>({...prev,[itemId]:1}))
+        } else {
+            
+        }
+    }
+
+
+
 
     const contextValue = {
         food_list
