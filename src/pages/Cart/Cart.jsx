621982d7355723683167fdb074c22dbe37c19aa7
Chauncey Darby <chaunceydrb@gmail.com>
2024-05-11 22:52:00 +0200

    updated

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -7,7 +7,7 @@ import { StoreContext } from '../../context/StoreContext'
 
 const Cart = () => {
 
-    const {cartItems,food_list,removeFromCart} = useContext(StoreContext);
+    
   
   return (
     <div className='cart'>
